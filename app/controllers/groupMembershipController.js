aca1700e3cec7dc33cc01dc7fa5dab67e25a3d2b
Enzo DJABALI <enzo.djabali@supinfo.com>
2024-03-06 17:03:12 +0100

    feat(#5): checking if the group is found

--- app/controllers/groupMembershipController.js
+++ app/controllers/groupMembershipController.js
@@ -1,11 +1,21 @@
 const GroupMembership = require('../models/GroupMembership');
 const User = require('../models/User');
+const Group = require('../models/Group');
 
 
 const createGroupMembership = async (req, res) => {
     const { user_id, group_id } = req.body;
 
     try {
+        // Find the group by its ID
+        const group = await Group.findById(group_id);
+
+        if (!group) {
+            return res.status(404).json({ message: "Group not found" });
+        }
+
+        // TODO: Check if the current user is the administrator of the group
+
         // Create a new group membership instance
         const newMembership = new GroupMembership({
             user_id,
@@ -20,6 +30,7 @@ const createGroupMembership = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
 
 module.exports = {
     createGroupMembership
