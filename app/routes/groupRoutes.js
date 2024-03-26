3dd720b555dd28b08bbe477a4217354b7daef684
lougautr <lougautr@gmail.com>
2024-03-26 13:13:38 +0100

    feat(#13): add deleteGroupById route + fix bug

--- app/routes/groupRoutes.js
+++ app/routes/groupRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createGroup, getAllGroups, getGroupById } = require ('../controllers/groupController');
+const { createGroup, getAllGroups, getGroupById, deleteGroupById } = require ('../controllers/groupController');
 const router = express.Router();
 
 const authenticateJWT = require('../middlewares/auth');
