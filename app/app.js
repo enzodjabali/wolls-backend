e06134ff2143b4a3ef0eceab0795bc3e31bd3afd
lougautr <lougautr@gmail.com>
2024-03-26 13:41:48 +0100

    bug(#16): add DELETE to CORS

--- app/app.js
+++ app/app.js
@@ -30,7 +30,7 @@ APP.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
     res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     next();
 });
 
