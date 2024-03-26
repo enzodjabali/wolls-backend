026ebfcf4f3b823f706dff9f0336854b941e8853
lougautr <lougautr@gmail.com>
2024-03-26 16:23:57 +0100

    bug(#24): 24-find-member-by-pseudo

--- app/controllers/groupMembershipController.js
+++ app/controllers/groupMembershipController.js
@@ -2,39 +2,34 @@ const GroupMembership = require('../models/GroupMembership');
 const User = require('../models/User');
 const Group = require('../models/Group');
 
-
 const createGroupMembership = async (req, res) => {
-    const { user_id, group_id } = req.body;
+    const { user_pseudonym, group_id } = req.body;
 
     try {
-        // Find the group by its ID
-        const group = await Group.findById(group_id);
+        // Trouver l'utilisateur par son pseudonyme
+        const user = await User.findOne({ pseudonym: user_pseudonym });
 
-        if (!group) {
-            return res.status(404).json({ message: "Group not found" });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
         }
 
-        // Check if the current user is the administrator of the group
-        const groupMembership = await GroupMembership.findOne({
-            user_id: req.userId,
-            group_id,
-            is_administrator: true
-        });
+        // Vérifier si l'utilisateur est déjà membre du groupe
+        const existingMembership = await GroupMembership.findOne({ user_id: user._id, group_id });
 
-        if (!groupMembership) {
-            return res.status(403).json({ message: "You are not authorized to add members to this group" });
+        if (existingMembership) {
+            return res.status(400).json({ message: "User is already a member of the group" });
         }
 
-        // Create a new group membership instance
+        // Créer une nouvelle instance d'adhésion au groupe
         const newMembership = new GroupMembership({
-            user_id,
+            user_id: user._id,
             group_id
         });
 
-        // Save the new group membership to the database
+        // Enregistrer la nouvelle adhésion au groupe dans la base de données
         const savedMembership = await newMembership.save();
 
-        res.status(201).json(savedMembership); // Respond with the saved group membership
+        res.status(201).json(savedMembership); // Répondre avec l'adhésion au groupe enregistrée
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
