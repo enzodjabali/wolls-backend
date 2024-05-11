1254642eb7e42b28a9242b46cc98cd5f6699f24b
lougautr <lougautr@gmail.com>
2024-05-11 15:29:49 +0200

    feat(#63): Get a specific expense by id

--- app/routes/expenseRoutes.js
+++ app/routes/expenseRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getExpenses ,createExpense, updateExpense, deleteExpense } = require ('../controllers/expenseController');
+const { getExpenses, getExpense, createExpense, updateExpense, deleteExpense } = require ('../controllers/expenseController');
 const router = express.Router();
 
 const authenticateJWT = require('../middlewares/auth');
 
 router.get('/:groupId', authenticateJWT, getExpenses);
+router.get('/:groupId/:expenseId', authenticateJWT, getExpense);
 router.post('/', authenticateJWT, createExpense);
 router.put('/:id', authenticateJWT, updateExpense);
 router.delete('/:id', authenticateJWT, deleteExpense);
