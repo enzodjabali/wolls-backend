d7c011061e5fa3e0e671175821fa581c42a7078e
Enzo DJABALI <enzo.djabali@supinfo.com>
2024-05-14 15:07:16 +0200

    feat(#87): implementing google auth

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { registerUser, authenticateUser, getUsersList, getCurrentUser, updateCurrentUser, updatePasswordCurrentUser, logoutUser, deleteCurrentUser, getUserById } = require ('../controllers/userController');
+const { registerUser, authenticateUser, getUsersList, getCurrentUser, updateCurrentUser, updatePasswordCurrentUser, logoutUser, deleteCurrentUser, getUserById, googleLogin } = require ('../controllers/userController');
 const router = express.Router();
 
 const authenticateJWT = require('../middlewares/auth');
 
 router.post('/register', registerUser);
 router.post('/login', authenticateUser);
+router.post('/login-google', googleLogin);
 router.get('/', authenticateJWT, getUsersList);
 router.get('/me', authenticateJWT, getCurrentUser);
 router.put('/', authenticateJWT, updateCurrentUser);
