bf310289209795b1e8664e2afd9052c11f96690e
Enzo DJABALI <enzo.djabali@supinfo.com>
2024-05-16 20:59:18 +0200

    tech(#118): commenting properly the middlewares

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -1,6 +1,12 @@
 const jwt = require("jsonwebtoken");
 const LOCALE = require('../locales/fr-FR');
 
+/**
+ * Middleware function to authenticate JSON Web Tokens (JWT) in request headers
+ * @param {Object} req The request object containing the JWT token in the 'Authorization' header
+ * @param {Object} res The response object to send back error messages if authentication fails
+ * @param {Function} next The next middleware function to call if authentication succeeds
+ */
 const authenticateJWT = (req, res, next) => {
     const token = req.header('Authorization');
 
--- app/middlewares/sendEmail.js
+++ app/middlewares/sendEmail.js
@@ -3,20 +3,31 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+/**
+ * Creates a nodemailer transporter object with the provided configuration
+ * @type {object}
+ */
 const transporter = nodemailer.createTransport({
     host: process.env.MAILER_HOST,
     port: process.env.MAILER_PORT,
-    secure: false, // true for 465, false for other ports
+    secure: false,
     auth: {
-        user: process.env.MAILER_USER, // Your OVH email address
-        pass: process.env.MAILER_PASS // Your OVH email password
+        user: process.env.MAILER_USER,
+        pass: process.env.MAILER_PASS
     }
 });
 
+/**
+ * Sends an email using the configured email transporter
+ * @param {string} email The email address of the recipient
+ * @param {string} subject The subject of the email
+ * @param {string} text The body text of the email
+ * @returns {boolean} Returns true if the email is sent successfully, otherwise false
+ */
 const sendEmail = async (email, subject, text) => {
     try {
         const mailOptions = {
-            from: process.env.MAILER_USER, // Your OVH email address
+            from: process.env.MAILER_USER,
             to: email,
             subject: subject,
             text: text
