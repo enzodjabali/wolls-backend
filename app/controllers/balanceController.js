34cdba1292df7dd4ce03a4b59c46edbfa4f186fc
Enzo DJABALI <enzo.djabali@supinfo.com>
2024-05-27 21:32:38 +0200

    feat(#142): returning pseudonyms instead of ids in balances

--- app/controllers/balanceController.js
+++ app/controllers/balanceController.js
@@ -1,14 +1,14 @@
 const Expense = require('../models/Expense');
 const GroupMembership = require('../models/GroupMembership');
+const User = require('../models/User'); // Make sure to require the User model
 const LOCALE = require('../locales/en-GB');
-
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 /**
  * Calculates the balances for each user in a group based on their expenses and refunds
  * @param {Object} req The request object containing parameters
  * @param {Object} res The response object to send back the balances
- * @returns {Object} Returns an object containing user IDs as keys and their corresponding balances
+ * @returns {Object} Returns an object containing user pseudonyms as keys and their corresponding balances
  */
 const getBalances = async (req, res) => {
     const groupId = req.params.groupId;
@@ -20,29 +20,41 @@ const getBalances = async (req, res) => {
 
         const expenses = await Expense.find({ group_id: groupId, isRefunded: false }).populate('refund_recipients');
 
-        const groupMemberships = await GroupMembership.find({ group_id: groupId }).populate('user_id');
+        const groupMemberships = await GroupMembership.find({ group_id: groupId }).populate('user_id', 'pseudonym');
 
         const balances = {};
         groupMemberships.forEach(membership => {
             const userId = membership.user_id._id.toString();
-            balances[userId] = 0;
+            const pseudonym = membership.user_id.pseudonym;
+            balances[userId] = { pseudonym, balance: 0 };
         });
 
-        expenses.forEach(expense => {
+        for (const expense of expenses) {
             const buyerId = expense.creator_id.toString();
+            const buyer = await User.findById(buyerId, 'pseudonym');
             const receivers = expense.refund_recipients.map(recipient => recipient._id.toString());
             const splitAmount = expense.amount / receivers.length;
 
-            if (!balances[buyerId]) balances[buyerId] = 0;
-            balances[buyerId] += expense.amount;
+            if (!balances[buyerId]) {
+                balances[buyerId] = { pseudonym: buyer.pseudonym, balance: 0 };
+            }
+            balances[buyerId].balance += expense.amount;
 
-            receivers.forEach(receiverId => {
-                if (!balances[receiverId]) balances[receiverId] = 0;
-                balances[receiverId] -= splitAmount;
-            });
-        });
+            for (const receiverId of receivers) {
+                if (!balances[receiverId]) {
+                    const recipient = await User.findById(receiverId, 'pseudonym');
+                    balances[receiverId] = { pseudonym: recipient.pseudonym, balance: 0 };
+                }
+                balances[receiverId].balance -= splitAmount;
+            }
+        }
 
-        res.status(200).json({ balances });
+        const result = {};
+        for (const { pseudonym, balance } of Object.values(balances)) {
+            result[pseudonym] = balance;
+        }
+
+        res.status(200).json({ balances: result });
     } catch (error) {
         console.error('Error fetching the balances:', error);
         res.status(500).json({ error: LOCALE.internalServerError });
