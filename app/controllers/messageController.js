0255587f593845c9660b272527d5b70ef4be2487
Enzo DJABALI <enzo.djabali@supinfo.com>
2024-06-26 16:37:35 +0300

    tech(#140): removing the unused code

--- app/controllers/messageController.js
+++ app/controllers/messageController.js
@@ -6,55 +6,45 @@ const LOCALE = require('../locales/en-EN');
 const { sendGroupMessageSchema } = require('../middlewares/validationSchema');
 
 const mongoose = require('mongoose');
-const { io, sendMessageToGroup } = require('../socket'); // Import the Socket.IO instance
-
-// Function to send a message to a group
-/**
-const sendMessageToGroup = (groupId, senderId, content) => {
-    const messageData = {
-        groupId: groupId,
-        id: new mongoose.Types.ObjectId().toString(),
-        senderId: senderId,
-        content: content,
-    };
-
-    // Emit the message to the group
-    if (io.to(groupId).emit('group_message', messageData)) {
-        console.log('Sent message from sendMessageToGroup function:', messageData);
-    } else {
-        console.log('cant send the message from api')
-    }
-};
-**/
-//module.exports = { sendMessageToGroup };
-
+const { io } = require('../socket'); // Import the Socket.IO instance
 
 const sendGroupMessage = async (req, res) => {
     try {
         await sendGroupMessageSchema.validateAsync(req.body, { abortEarly: false });
 
         const { groupId, content } = req.body;
-        console.log(groupId)
+        const senderId = req.userId; // Assume req.userId contains the ID of the authenticated user
 
-        const group = await Group.findById(groupId);
-        const senderId = req.userId;
-        const isMember = await GroupMembership.exists({ user_id: senderId, group_id: groupId });
-
+        // Validate groupId
         if (!mongoose.Types.ObjectId.isValid(groupId)) {
             return res.status(400).json({ error: LOCALE.groupNotFound });
         }
 
+        // Check if the group exists
+        const group = await Group.findById(groupId);
         if (!group) {
             return res.status(404).json({ error: LOCALE.groupNotFound });
         }
 
+        // Check if the user is a member of the group
+        const isMember = await GroupMembership.exists({ user_id: senderId, group_id: groupId });
         if (!isMember) {
             return res.status(403).json({ error: LOCALE.notGroupMember });
         }
 
-        sendMessageToGroup(groupId, senderId, content);
+        // Create a new message and save it to the database
+        const newMessage = new Message({
+            groupId: groupId,
+            senderId: senderId,
+            content: content
+        });
 
-        res.status(201).json({ success: true });
+        await newMessage.save();
+
+        // Send the message to the group via Socket.IO
+        io.to(groupId).emit('group_message', newMessage)
+
+        res.status(201).json(newMessage);
     } catch (error) {
         if (error.name === 'ValidationError') {
             const errorMessage = error.details.map(detail => detail.message).join(', ');
--- app/socket.js
+++ app/socket.js
@@ -16,30 +16,11 @@ const io = socketIO(server, {
     },
 });
 
-// Export the server and io instance
-
-
 // Start the server on port 3002
 const PORT_SOCKET = 3002;
 server.listen(PORT_SOCKET, () => {
-    console.log(`Server is running on port ${PORT_SOCKET}`);
+    console.log(`Socket.io is running on port ${PORT_SOCKET}`);
 });
 
-const sendMessageToGroup = (groupId, senderId, content) => {
-    console.log(groupId)
-    const messageData = {
-        groupId: groupId,
-        id: new mongoose.Types.ObjectId().toString(),
-        senderId: senderId,
-        content: content,
-    };
-
-    // Emit the message to the group
-    if (io.to(groupId).emit('group_message', messageData)) {
-        console.log('Sent message from sendMessageToGroup function:', messageData);
-    } else {
-        console.log('cant send the message from api')
-    }
-};
-
-module.exports = { server, io, sendMessageToGroup };
+// Export the server and io instance
+module.exports = { server, io };
